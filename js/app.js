eee5ec436513b970fcdb6b0138ae758e3e55035f
전혜린 <jeonhyerin@hivelab.co.kr>
2023-10-28 22:10:44 +0900

    2.7 recap

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
+const modeBtn = document.querySelector('#mode-btn');
+const destroyBtn = document.querySelector('#destroy-btn');
+const eraserBtn = document.querySelector('#eraser-btn');
+const colorOptions = Array.from(document.getElementsByClassName('color-option'));
+const lineWidth = document.querySelector('#line-width');
+const color = document.querySelector('#color');
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
+let isFilling = false;
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
 
 function onMove(event) {
   if(isPainting) {
@@ -19,9 +30,60 @@ function startPainting() {
 
 function cancelPainting() {
   isPainting = false;
+  ctx.beginPath();
+}
+
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+  ctx.strokeStyle = ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = ctx.fillStyle = event.target.dataset.color;
+  color.value = colorValue;
+}
+
+function onModeClick() {
+  if(isFilling) {
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+  } else {
+    isFilling = true;
+    modeBtn.innerText = "Draw";
+  }
+}
+
+function onCanvasClick() {
+  if(isFilling) {
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+}
+
+function onDestoryClick() {
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraserClick() {
+  ctx.strokeStyle = 'white';
+  isFilling = false;
+  modeBtn.innerText = "Fill";
 }
 
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', startPainting);
 canvas.addEventListener('mouseup', cancelPainting);
 canvas.addEventListener('mouseleave', cancelPainting);
+canvas.addEventListener('click', onCanvasClick);
+lineWidth.addEventListener('change', onLineWidthChange);
+color.addEventListener('change', onColorChange);
+
+colorOptions.forEach(color => color.addEventListener('click', onColorClick));
+
+modeBtn.addEventListener('click', onModeClick);
+destroyBtn.addEventListener('click', onDestoryClick);
+eraserBtn.addEventListener('click', onEraserClick)
